dc894830fd507dc03faf68514529cfc693c6f775
Har-Tsan <niahartan@gmail.com>
2019-07-20 22:47:46 +0700

    Update No3.php

--- No3.php
+++ No3.php
@@ -1,15 +1,36 @@
 <?php
- $list_string = array('pro','gram','merit','program','it','programmer');
 
-$kalimat = 'programit';
-$checklist = array();
-foreach ($list_string as $s) {
-  if (strpos($kalimat, $s) !== false) {
-      echo $s." ";
-      array_push($checklist,$s);
+  function splitstring($kalimat)
+  {
+    $list_string = array('pro','program','programmer','gram','it','merit');
+    $checklist = array();
+    $result = array();
+    foreach ($list_string as $s) {
+       if (strpos($kalimat, $s) !== false) {
+           echo $s." ";
+           array_push($checklist,$s);
+       }
+     }
+     foreach ($checklist as $c) {
+       $listing = array();
+       if(substr($kalimat,0,strlen($c)) == $c) {
+           array_push($listing,$c);
+           $mark = strlen($c);
+           //3
+           foreach ($checklist as $ch) {
+             //3-
+             if(substr($kalimat,$mark,strlen($ch)) == $ch) {
+               array_push($listing,$ch);
+               $mark = $mark + strlen($ch);
+             }
+           }
+           array_push($result,$listing);
+       }
+     }
+     return $result;
   }
-  
-}
 
-
+  $kalimat = 'programmerit';
+  $result = splitstring($kalimat);
+  print_r($result);
 ?>
